2839bb0ed6ec12d1bad0f7f0f9bb3035929c125f
Lin可涵 <hermender@outlook.com>
2019-02-23 23:22:17 +0800

    更新

---
+++ 1.js
@@ -0,0 +1,30 @@
+
+
+
+
+class Robot {
+    fly(distance) {
+        if (distance && distance > 0) {
+            console.log("飞行", distance, '米');
+        }
+        else if (distance && distance <= 0){
+            console.log("坠机");
+        }
+        else {
+            console.log("垂直飞");
+        }
+
+    }
+}
+
+
+const robot = new Robot();
+
+
+robot.fly(-1);
+robot.fly();
+robot.fly(23542342);
+
+
+
+
